41124d0f1dfde3a02e00c8b6ae3b5f2b9b2b9c91
Damien Altenburg <daltenburg@rrc.ca>
2025-01-22 14:52:30 -0600

    Initial commit.

---
+++ module-2/module_02_data_types.py
@@ -0,0 +1,17 @@
+"""This module demonstrates data types for module 2."""
+
+__author__ = "Damien Altenburg"
+__version__ = "1.0.0"
+
+# Syntax: Declaring a variable
+# identifier = value
+name = "Damien Altenburg"
+age = 25
+annual_income = 67890.45
+is_employed = True
+
+# This identifier is not well named and is only for demonstration sake
+value_type = type(25)
+print(value_type)
+
+print(type(25))
