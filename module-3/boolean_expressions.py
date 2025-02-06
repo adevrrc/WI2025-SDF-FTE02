9774e77006e50f2a6ba5d719bee07e8bd07e0f38
Damien Altenburg <daltenburg@rrc.ca>
2025-02-06 07:33:58 -0600

    Update to demonstrate boolean expressions.

--- module-3/boolean_expressions.py
+++ module-3/boolean_expressions.py
@@ -4,5 +4,21 @@ Example:
     $ python boolean_expressions.py
 """
 
-__author__ = "your_name_here"
+__author__ = "Damien Altenburg"
 __version__ = "1.0.0"
+
+age = 25
+name = "Damien"
+
+print(True)
+print(False)
+
+is_person_cool = True
+
+print(is_person_cool)
+
+print(name.isalpha())
+
+print(age == 25)
+print(age != 25)
+print(age < 30)
