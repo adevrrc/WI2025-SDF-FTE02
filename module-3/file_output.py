3c9a99087d3f875d36b36874d0d79546df562534
Damien Altenburg <daltenburg@rrc.ca>
2025-02-07 16:29:20 -0600

    Initial commit.

--- module-3/file_output.py
+++ module-3/file_output.py
@@ -10,3 +10,22 @@ __version__ = "1.0.0"
 FILE_PATH = "grades.txt"
 
 grades = [85, 34, 67, 78, 89, 99, 21, 45, 64, 88, 92]
+
+
+# Write to a file then close the file
+with open(FILE_PATH, "w") as file:
+    file.write(str(grades[0]))
+
+# Write to file using List
+with open(FILE_PATH, "w") as file:
+    for grade in grades:
+        # Writes without a line ending
+        #file.write(str(grade))
+
+        # Write with line ending
+        file.write(f"{grade}\n")
+
+# Append to a file
+with open(FILE_PATH, "a") as file:
+    for grade in [66, 77, 88]:
+        file.write(f"{grade}\n")
--- module-3/list_comprehension.py
+++ module-3/list_comprehension.py
@@ -8,3 +8,9 @@ __author__ = "your_name_here"
 __version__ = "1.0.0"
 
 names = ["kenny", "eric", "kyle", "stan", "butters", "clyde"]
+
+squares = [number ** 2 for number in range(1, 11)]
+print(squares)
+
+odd_names = [name.title() for name in names if len(name) % 2 != 0]
+print(odd_names)
