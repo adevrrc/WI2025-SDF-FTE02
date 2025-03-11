e7671b9d0e2710df8991137c205b589a26522709
Damien Altenburg <daltenburg@rrc.ca>
2025-03-11 15:40:56 -0500

    Update to demonstration function standards.

--- module-5/function_standards.py
+++ module-5/function_standards.py
@@ -8,6 +8,21 @@ __version__ = "1.11.2024"
 __author__ = "Damien Altenburg"
 
 def format_greeting(first_name: str, last_name: str, salutation="Hello", title="Mr.") -> str:
+    """Returns a formatted greeting with the specified names.
+    
+    Args:
+        first_name (str): The person's first name.
+        last_name (str): The person's last name.
+        ...
+        
+    Returns:
+        str: A formatted greeting with the specified names.
+    
+    Example:
+        >>> format_greeting("Damien", "Altenburg")
+        Hello, Mr. Damien Altenburg
+    """
+
     full_name = f"{first_name} {last_name}".strip()
 
     return f"{salutation}, {title} {full_name}"
