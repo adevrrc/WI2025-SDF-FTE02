cdf0a698a02e9cb41ad5b275c3e71ee24f6f160f
Damien Altenburg <daltenburg@rrc.ca>
2025-03-26 08:23:18 -0500

    Update to demostrate using a class.

--- module-6/main.py
+++ module-6/main.py
@@ -1,10 +1,31 @@
 """This module is used to demonstrate using a class."""
 
+from playing_card.playing_card import PlayingCard
+
 __author__ = "Damien Altenburg"
 __version__ = "1.3.2025"
 
 def main():
-    pass
+    """"""
+
+    playing_card = PlayingCard(0, "Heart")
+
+    print(playing_card)
+
+    playing_card = PlayingCard(6, "Club")
+
+    print(playing_card)
+
+    playing_card.suit = "Heart"
+    playing_card.rank = 10
+
+    print(playing_card.rank)
+    print(playing_card.suit)
+
+    playing_card.flip()
+
+    print(repr(playing_card))
+    print(str(playing_card))
 
 if __name__ == "__main__":
     main()
