9d0334efe7246cf6d42b2084eada184421d79c2a
Damien Altenburg <daltenburg@rrc.ca>
2025-03-26 08:22:58 -0500

    Update to declare and define a class.

--- module-6/playing_card/playing_card.py
+++ module-6/playing_card/playing_card.py
@@ -2,4 +2,63 @@
 
 __author__ = "Damien Altenburg"
 __version__ = "1.3.2025"
+
+class PlayingCard:
+    """"""
+    
+    def __init__(self, rank: int, suit: str):
+        """"""
+
+        self.__rank = rank
+        self.__suit = suit
+        self.__is_face_up = False
+
+    @property
+    def rank(self) -> int:
+        """"""
+
+        return self.__rank
+    
+    @rank.setter
+    def rank(self, rank: int) -> None:
+        """"""
+
+        self.__rank = rank
+
+    @property
+    def suit(self) -> str:
+        """"""
+
+        return self.__suit
+    
+    @suit.setter
+    def suit(self, suit: str) -> None:
+        """"""
+
+        self.__suit = suit
+
+    @property
+    def is_face_up(self) -> bool:
+        """"""
+
+        return self.__is_face_up
+
+    def flip(self) -> None:
+        # if self.__is_face_up:
+        #     self.__is_face_up = False
+        # else:
+        #     self.__is_face_up = True
+
+        self.__is_face_up = not self.__is_face_up
+
+    def __repr__(self) -> str:
+        """"""
+
+        return f"PlayingCard({self.__rank}, {self.__suit})"
+    
+    def __str__(self) -> str:
+        """"""
+
+        return f"{self.__rank} {self.__suit}"
+    
     
